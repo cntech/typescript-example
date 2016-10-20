ef1f51ea1d233ef80cc1f2ad11a5f768c5deb881
niederreiter.tech <christian@niederreiter.tech>
2016-10-20 14:08:34 +0200

    style(defaultmessage): missing semicolon

--- src/lib/default-message.ts
+++ src/lib/default-message.ts
@@ -3,7 +3,7 @@ import Message from './message';
 class DefaultMessage implements Message {
     constructor(private _messageText: string) {}
     get messageText(): string {
-      return this._messageText
+      return this._messageText;
     }
     public print() {
         console.log(this.messageText);
